3ae1221bf5962deb6d6ee857e6456443f46f4804
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-06 20:37:23 +0330

    promise #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const metting = new Promise((resolve, reject) => {
   if (!hassmeeting) {
     const meetingdetails = {
       name: "Codyad meeting",
-      olcation: "Tehran",
+      location: "Tehran",
       time: "01:00 PM",
     };
     resolve(meetingdetails);
@@ -11,4 +11,12 @@ const metting = new Promise((resolve, reject) => {
     reject(new Error("meeting canseled"));
   }
 });
+
+const aDd = (meetingdetails) => {
+  return new Promise((res, rej) => {
+    const calender = `${meetingdetails.name} is scheduled at ${meetingdetails.time} on ${meetingdetail.location}`;
+    res(calender);
+  });
+};
+
 metting.then((res) => console.log(res)).catch((rej) => console.log(rej));
